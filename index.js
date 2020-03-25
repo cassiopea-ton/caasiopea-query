cdf6715303727ee41aaa0e669a9dd7c9af94b869
kstasi <nastasiakondaurova@gmail.com>
2020-03-25 17:23:09 +0200

    cell decerializer implemented

--- index.js
+++ index.js
@@ -35,25 +35,24 @@ class QueryClient {
     );
   }
 }
-
-// boc: {
-//     magic: Int,
-//     has_idx: Boolean,
-//     has_crc32c: Boolean,
-//     has_cache_bits: Boolean,
-//     flags: Int,
-//     size: Int,
-//     off_bytes: Int,
-//     cells: Int,
-//     roots: Int,
-//     absent: Int,
-//     tot_cells_size: Int,
-//     root_list: dataBinary,
-//     index: Boolean,
-//     cell_data: dataBinary
-//     crc32c: Int
-// }
 class DataDeserializer {
+  // boc: {
+  //     magic: Int,
+  //     has_idx: Boolean,
+  //     has_crc32c: Boolean,
+  //     has_cache_bits: Boolean,
+  //     flags: Int,
+  //     size: Int,
+  //     off_bytes: Int,
+  //     cells: Int,
+  //     roots: Int,
+  //     absent: Int,
+  //     tot_cells_size: Int,
+  //     root_list: dataBinary,
+  //     index: Boolean,
+  //     cell_data: dataBinary
+  //     crc32c: Int
+  // }
   static deserializeBoc(boc) {
     let decerializedBoc = {};
     decerializedBoc.magic = boc.readUIntBE(0, 4);
@@ -93,14 +92,44 @@ class DataDeserializer {
         (offset += decerializedBoc.cells * decerializedBoc.off_bytes)
       );
     }
-    decerializedBoc.data = boc.slice(
-      offset,
-      (offset += decerializedBoc.tot_cells_size)
+    decerializedBoc.data = DataDeserializer.deserializeCells(
+      boc.slice(offset, (offset += decerializedBoc.tot_cells_size)),
+      decerializedBoc.cells,
+      decerializedBoc.size
     );
     if (decerializedBoc.has_crc32c) {
       decerializedBoc.index = boc.readUIntBE(offset, 4);
     }
     return decerializedBoc;
+  }
+  static deserializeCells(data, counter, size) {
+    let cells = [];
+    let offset = 0;
+    for (let cell_idx = 0; cell_idx < counter; cell_idx++) {
+      cells[cell_idx] = {};
+      cells[cell_idx].refs = data[offset] & 7;
+      cells[cell_idx].is_exotic = data[offset] & 8;
+      cells[cell_idx].has_hash = data[offset] & 16;
+      cells[cell_idx].level = data[offset] & (3 << 5);
+
+      offset += 1;
+      cells[cell_idx].data_size = (data[offset] >> 1) + (data[offset] & 1);
+      cells[cell_idx].not_full = data[offset] & 1;
+      cells[cell_idx].full_data_size =
+        2 + cells[cell_idx].data_size + cells[cell_idx].refs * size;
+
+      offset += 1;
+      cells[cell_idx].data = data.slice(
+        offset,
+        (offset += cells[cell_idx].data_size)
+      );
+      cells[cell_idx].references = [];
+      for (let ref = 0; ref < cells[cell_idx].refs; ref++) {
+        cells[cell_idx].references.push(data.readUIntBE(offset, size));
+        offset += size;
+      }
+    }
+    return cells;
   }
 }
 
