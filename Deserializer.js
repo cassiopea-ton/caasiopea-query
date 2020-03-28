9c7d295b9314be6337154bf07c4dab288af8fd31
kstasi <nastasiakondaurova@gmail.com>
2020-03-28 11:07:02 +0200

    deserialize draft

--- Deserializer.js
+++ Deserializer.js
@@ -1,3 +1,127 @@
+class Deserializer {
+  deserialize(cell, abi) {}
+}
+
+// ref
+// int
+// uint
+// bits
+// grams
+// op_ref
+// dict
+
+// {
+//      "type": "ref",
+//      "body": { ... }
+// }
+
+// {
+//      "type": "op_ref",
+//      "body": { ... }
+// }
+
+// {
+//      "type": "int",
+//      "size": uint
+// }
+
+// {
+//      "type": "uint",
+//      "size": uint
+// }
+
+// {
+//      "type": "bits",
+//      "size": uint
+// }
+
+// {
+//      "type": "grams"
+// }
+
+// {
+//      "type": "dict",
+//      "key": { ... },
+//      "value": { ... },
+// }
+
+class CellData {
+  constructor(data, references) {
+    this.data = data;
+    this.references = references;
+    this.offset = 0;
+    this.refOffset = 0;
+  }
+
+  readBits(data, counter) {
+    let byteStart = Math.floor(this.offset / 8);
+    let length = Math.ceil(counter / 8);
+    let bitStart = this.offset - byteStart * 8;
+    let dataSlice = data.readUIntBE(byteStart, length);
+    let mask = 0;
+    for (let i = 0; i < counter; i++) {
+      mask |= 1 << (length * 8 - 1 - bitStart - i);
+    }
+    this.offset += counter;
+    let padding = 8 - ((bitStart + counter) % 8);
+    return padding === 8 ? dataSlice & mask : (dataSlice & mask) >> padding;
+  }
+
+  static toggleBits(dataSlice, counter) {
+    for (let i = 0; i < counter; i++) {
+      dataSlice ^= 1 << i;
+    }
+    return dataSlice;
+  }
+
+  readUint(data, counter) {
+    let sign = this.readBits(data, 1) ? -1 : 1;
+    let int = this.readBits(data, counter - 1);
+    if (sign === -1) {
+      int = CellData.toggleBits(int, counter - 1) + 1;
+    }
+    return sign * int;
+  }
+  readDict(cell, keyLength) {
+    let dict = {};
+    console.log(cell);
+    return dict;
+  }
+
+  deserialize(abi) {
+    let result = [];
+    abi.forEach(item => {
+      switch (item.type) {
+        case "ref":
+          break;
+        case "uint":
+          result.push(this.readUint(this.data, item.size));
+          break;
+        case "int":
+        case "bits":
+          result.push(this.readBits(this.data, item.size));
+          break;
+        case "grams":
+          break;
+        case "op_ref":
+          break;
+        case "dict":
+          if (this.readBits(this.data, 1)) {
+            let dict = this.readDict(
+              this.references[this.refOffset],
+              item.key.size
+            );
+            result.push(dict);
+          } else {
+            result.push({});
+          }
+          break;
+      }
+    });
+    return result;
+  }
+}
+
 class Cell {
   constructor(data, offsetObj, ref_size) {
     let offset = offsetObj.offset;
@@ -13,33 +137,16 @@ class Cell {
     this.data = data.slice(offset, (offset += this.data_size));
     this.references = [];
     for (let ref = 0; ref < this.refs_counter; ref++) {
-      this.references.push(data.readUIntBE(offset, size));
-      offset += size;
+      this.references.push(data.readUIntBE(offset, ref_size));
+      offset += ref_size;
     }
     offsetObj.offset = offset;
   }
 }
 
 class BagOfCells {
-  constructor() {
-    this.magic = 0;
-    this.has_idx = 0;
-    this.has_crc32c = 0;
-    this.has_cache_bits = 0;
-    this.flags = 0;
-    this.size = 0;
-    this.off_bytes = 0;
-    this.cells = 0;
-    this.roots = 0;
-    this.absent = 0;
-    this.tot_cells_size = 0;
-    this.root_list = 0;
-    this.index = 0;
-    this.cell_data = 0;
-    this.crc32c = 0;
-  }
-
   constructor(boc) {
+    //   read general info
     this.magic = boc.readUIntBE(0, 4);
 
     let b = boc[4];
@@ -69,16 +176,45 @@ class BagOfCells {
       this.index = boc.slice(offset, (offset += this.cells * this.off_bytes));
     }
 
+    // manage cells
     this.cell_data = [];
-    for (let cell_idx = 0; cell_idx < counter; cell_idx++) {
+    this.cell_data_slice = [];
+    for (let cell_idx = 0; cell_idx < this.cells; cell_idx++) {
       let offsetObj = { offset };
       this.cell_data.push(
         new Cell(boc, offsetObj, this.ref_size, cell_idx, this.cells)
       );
-      (offset = offsetObj), offset;
+      this.cell_data_slice.push({
+        data: Buffer.from(this.cell_data[cell_idx].data),
+        references_data: []
+      });
+      offset = offsetObj.offset;
     }
+
+    // fucking muddle with refs
+    for (let cell_idx = this.cells - 1; cell_idx >= 0; cell_idx--) {
+      this.cell_data[cell_idx].references_data = [];
+      let references = this.cell_data[cell_idx].references;
+      for (let ref = 0; ref < references.length; ref++) {
+        this.cell_data[cell_idx].references_data.push({
+          ...this.cell_data[references[ref]]
+        });
+
+        this.cell_data_slice[cell_idx].references_data.push(
+          this.cell_data_slice[references[ref]]
+        );
+      }
+      this.cell_data_slice[cell_idx] = new CellData(
+        this.cell_data_slice[cell_idx].data,
+        this.cell_data_slice[cell_idx].references_data
+      );
+    }
+
     if (this.has_crc32c) {
       this.index = boc.readUIntBE(offset, 4);
     }
   }
 }
+
+module.exports.Cell = Cell;
+module.exports.BagOfCells = BagOfCells;
--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const { TONClient } = require("ton-client-node-js");
+const { BagOfCells } = require("./Deserializer");
+
+let abi = [
+  {
+    type: "dict",
+    key: { type: "uint", size: 8 },
+    value: { type: "ref", body: {} }
+  }
+];
 
 class QueryClient {
   constructor(client) {
@@ -36,114 +45,6 @@ class QueryClient {
   }
 }
 class DataDeserializer {
-  // boc: {
-  //     magic: Int,
-  //     has_idx: Boolean,
-  //     has_crc32c: Boolean,
-  //     has_cache_bits: Boolean,
-  //     flags: Int,
-  //     size: Int,
-  //     off_bytes: Int,
-  //     cells: Int,
-  //     roots: Int,
-  //     absent: Int,
-  //     tot_cells_size: Int,
-  //     root_list: dataBinary,
-  //     index: Boolean,
-  //     cell_data: dataBinary
-  //     crc32c: Int
-  // }
-  static deserializeBoc(boc) {
-    let decerializedBoc = {};
-    decerializedBoc.magic = boc.readUIntBE(0, 4);
-
-    let b = boc[4];
-    decerializedBoc.has_idx = b & (1 << 7);
-    decerializedBoc.has_crc32c = b & (1 << 6);
-    decerializedBoc.has_cache_bits = b & (1 << 5);
-    decerializedBoc.flags = b & (3 << 3);
-    decerializedBoc.size = b & 7;
-    decerializedBoc.off_bytes = boc[5];
-
-    let offset = 6;
-    decerializedBoc.cells = boc.readUIntBE(offset, decerializedBoc.size);
-    offset += decerializedBoc.size;
-
-    decerializedBoc.roots = boc.readUIntBE(offset, decerializedBoc.size);
-    offset += decerializedBoc.size;
-
-    decerializedBoc.absent = boc.readUIntBE(offset, decerializedBoc.size);
-    offset += decerializedBoc.size;
-
-    decerializedBoc.tot_cells_size = boc.readUIntBE(
-      offset,
-      decerializedBoc.off_bytes
-    );
-    offset += decerializedBoc.off_bytes;
-
-    decerializedBoc.root_list = boc.slice(
-      offset,
-      (offset += decerializedBoc.roots * decerializedBoc.size)
-    );
-
-    if (decerializedBoc.has_idx) {
-      decerializedBoc.index = boc.slice(
-        offset,
-        (offset += decerializedBoc.cells * decerializedBoc.off_bytes)
-      );
-    }
-    decerializedBoc.data = DataDeserializer.deserializeCells(
-      boc.slice(offset, (offset += decerializedBoc.tot_cells_size)),
-      decerializedBoc.cells,
-      decerializedBoc.size
-    );
-    if (decerializedBoc.has_crc32c) {
-      decerializedBoc.index = boc.readUIntBE(offset, 4);
-    }
-    return decerializedBoc;
-  }
-  static deserializeCells(data, counter, size) {
-    let cells = [];
-    let offset = 0;
-    for (let cell_idx = 0; cell_idx < counter; cell_idx++) {
-      cells[cell_idx] = {};
-      cells[cell_idx].refs = data[offset] & 7;
-      cells[cell_idx].is_exotic = data[offset] & 8;
-      cells[cell_idx].has_hash = data[offset] & 16;
-      cells[cell_idx].level = data[offset] & (3 << 5);
-
-      offset += 1;
-      cells[cell_idx].data_size = (data[offset] >> 1) + (data[offset] & 1);
-      cells[cell_idx].not_full = data[offset] & 1;
-      cells[cell_idx].full_data_size =
-        2 + cells[cell_idx].data_size + cells[cell_idx].refs * size;
-
-      offset += 1;
-      cells[cell_idx].data = data.slice(
-        offset,
-        (offset += cells[cell_idx].data_size)
-      );
-      cells[cell_idx].references = [];
-      for (let ref = 0; ref < cells[cell_idx].refs; ref++) {
-        cells[cell_idx].references.push(data.readUIntBE(offset, size));
-        offset += size;
-      }
-    }
-    return cells;
-  }
-
-  static readBits(data, bitIndex, counter) {
-    let dataSlice = data.readUIntBE(~~(bitIndex / 8), ~~(counter / 8) + 1);
-    let mask = 0;
-    let bitStart = bitIndex % 8;
-    for (let i = 0; i < counter; i++) {
-      mask |= 1 << (~~(bitIndex / 8) * 8 + 7 - (bitIndex % 8) - i);
-    }
-    return (dataSlice & mask) >> (8 - (bitIndex % 8) - counter);
-  }
-
-  static getLabel() {}
-
   static deserializeDict(dictData, data, keyLength) {
     let bitOffset = 0;
     let dict = {};
@@ -195,36 +96,6 @@ class DataDeserializer {
 
     return dict;
   }
-
-  static deserializeData(data, rootIdx, abi) {
-    let offset = 0;
-    let bitOffset = 0;
-    let refOffset = 0;
-    let struct = [];
-    let root = data[rootIdx];
-    abi.forEach((item, index) => {
-      switch (item.type) {
-        case "dict":
-          let hme = root.data[Math.floor(bitOffset++ / 8)];
-          if (hme) {
-            struct.push(
-              DataDeserializer.deserializeDict(
-                data[root.references[refOffset++]],
-                data,
-                item.keys.length
-              )
-            );
-          } else {
-            struct.push({});
-          }
-          break;
-        case "grams":
-          break;
-      }
-    });
-
-    return struct;
-  }
 }
 
 async function main(client) {
@@ -234,29 +105,17 @@ async function main(client) {
   const account = await queryClient.getAccount(registerAddr);
 
   const buffer = Buffer.from(account[0].data, "base64");
-  let contractStorage = DataDeserializer.deserializeBoc(buffer);
-  let abi = [
-    {
-      type: "dict",
-      keys: { type: "uint", length: 8 },
-      value: { type: "slice" }
-    },
-    {
-      type: "dict",
-      keys: { type: "uint", length: 8 },
-      value: { type: "slice" }
-    },
-    {
-      type: "dict",
-      keys: { type: "uint", length: 8 },
-      value: { type: "slice" }
-    },
-    {
-      type: "grams"
-    }
-  ];
+  // let contractStorage = DataDeserializer.deserializeBoc(buffer);
+  let testAbi = [];
+  for (let i = 0; i < 1; i++) {
+    testAbi.push({
+      type: "uint",
+      size: 4
+    });
+  }
+  let c = new BagOfCells(buffer);
+  console.log(c.cell_data_slice[0].deserialize(abi));
 
-  console.log(DataDeserializer.deserializeData(contractStorage.data, 0, abi));
   let dataBinary = buffer.reduce((binStr, el) => {
     return binStr + el.toString(2).padStart(8, "0");
   }, "");
